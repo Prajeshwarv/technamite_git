51bf40434aba33f8d2da0834de87c23a20c0e213
Prajeshwar <prajeshwarv382@gmail.com>
2021-04-14 16:34:50 +0530

    home

--- technamiteapp/src/app/app-routing.module.ts
+++ technamiteapp/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomepageModule } from './homepage/homepage.module';
+import { HomepageComponent } from './homepage/homepage/homepage.component';
+
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'contact', component: HomepageComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' }), HomepageModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- technamiteapp/src/app/homepage/homepage.module.ts
+++ technamiteapp/src/app/homepage/homepage.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
-    declarations: [],
+    declarations: [NavbarComponent, HomepageComponent, ContactComponent],
 
     imports: [
         CommonModule,
